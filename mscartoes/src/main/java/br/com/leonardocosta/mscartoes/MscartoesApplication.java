e80dd261ce051a38dda6b7617fd9ce15d8891869
Leonardo Costa <devleonardo27@gmail.com>
2025-06-11 15:41:52 -0300

    feat: add keycloak

--- mscartoes/src/main/java/br/com/leonardocosta/mscartoes/MscartoesApplication.java
+++ mscartoes/src/main/java/br/com/leonardocosta/mscartoes/MscartoesApplication.java
@@ -1,9 +1,11 @@
 package br.com.leonardocosta.mscartoes;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableRabbit
 @SpringBootApplication
 @EnableEurekaClient
 public class MscartoesApplication {
--- mscartoes/src/main/java/br/com/leonardocosta/mscartoes/infra/mqqueue/EmissaoCartoesSubscriber.java
+++ mscartoes/src/main/java/br/com/leonardocosta/mscartoes/infra/mqqueue/EmissaoCartoesSubscriber.java
@@ -1,4 +1,39 @@
 package br.com.leonardocosta.mscartoes.infra.mqqueue;
 
+import br.com.leonardocosta.mscartoes.application.port.out.CartaoRepository;
+import br.com.leonardocosta.mscartoes.application.port.out.ClienteCartaoRepository;
+import br.com.leonardocosta.mscartoes.domain.model.Cartao;
+import br.com.leonardocosta.mscartoes.domain.model.ClienteCartao;
+import br.com.leonardocosta.mscartoes.domain.model.DadosSolicitacaoEmissaoCartao;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
 public class EmissaoCartoesSubscriber {
+
+    private final CartaoRepository cartaoRepository;
+    private final ClienteCartaoRepository clienteCartaoRepository;
+
+    @RabbitListener(queues = "${mq.queues.emissao-cartoes}")
+    public void receberSolicitacaoEmissao(@Payload String payload) {
+        try {
+            var mapper = new ObjectMapper();
+            DadosSolicitacaoEmissaoCartao dados = mapper.readValue(payload, DadosSolicitacaoEmissaoCartao.class);
+            Cartao cartao = cartaoRepository.findById(dados.getIdCartao()).orElseThrow();
+            ClienteCartao clienteCartao = new ClienteCartao();
+            clienteCartao.setCartao(cartao);
+            clienteCartao.setCpf(dados.getCpf());
+            clienteCartao.setLimite(dados.getLimiteLiberado());
+
+            clienteCartaoRepository.save(clienteCartao);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
